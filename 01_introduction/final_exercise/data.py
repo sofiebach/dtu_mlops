d431b5c6f8ce8490ae98bd73c13941dcaa884471
sofiebach <70587917+sofiebach@users.noreply.github.com>
2021-06-07 10:48:34 +0200

    test

--- 01_introduction/final_exercise/data.py
+++ 01_introduction/final_exercise/data.py
@@ -3,5 +3,5 @@ import torch
 def mnist():
     # exchange with the real mnist dataset
     train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
+    test = torch.randn(10000, 785) 
     return train, test
